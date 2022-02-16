534ec057c59dcc9f410f2632fac48104935a9d66
david graff <deg711@g.harvard.edu>
2022-02-16 13:38:31 -0500

    switching over build system

--- scripts/make_all_figs.sh
+++ scripts/make_all_figs.sh
@@ -1,4 +1,8 @@
-python scripts/figures.py michalewicz ./data/all-objs/michalewicz ./data/michalewicz-gamma -o figures/github/michal-figure.png 
-python scripts/figures.py combo ./data/all-objs/* -o figures/github/combo.png
-python scripts/figures.py regret ./data/all-objs/michalewicz michalewicz -o figures/github/mich-regret.png
-python scripts/figures.py gamma-perf ./data/michalewicz-gamma michalewicz -o figures/github/mich-gamma-perf.png
+#!/bin/bash
+
+DIR=${1:-./figures}
+
+python scripts/figures.py michalewicz ./data/all-objs/michalewicz ./data/michalewicz-gamma -o $DIR/michal-figure.png 
+python scripts/figures.py combo ./data/all-objs/* -o $DIR/combo.png
+python scripts/figures.py regret ./data/all-objs/michalewicz michalewicz -o $DIR/mich-regret.png
+python scripts/figures.py gamma-perf ./data/michalewicz-gamma michalewicz -o $DIR/mich-gamma-perf.png
